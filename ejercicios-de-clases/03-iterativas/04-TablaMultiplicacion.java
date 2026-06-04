c2113f8e39ecc71d34040daae759a9144b9c4925
Logan <mattahuasi@gmail.com>
2026-06-03 21:22:42 -0400

    corregido numero de tabla n -> i

--- ejercicios-de-clases/03-iterativas/04-TablaMultiplicacion.java
+++ ejercicios-de-clases/03-iterativas/04-TablaMultiplicacion.java
@@ -10,7 +10,7 @@ class TablaMultiplicacion {
 
         System.out.println();
         for (int i = 1; i <= n; i++) {
-            System.out.println("Tabla de multiplicar del: " + n);
+            System.out.println("Tabla de multiplicar del: " + i);
             for (int j = 1; j <= 10; j++) {
                 System.out.println(i + " * " + j + " = " + i * j);
             }
